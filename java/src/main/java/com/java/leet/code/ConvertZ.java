10839b9dcea5d55d2812433d38d2d749e3c47bcf
Ning <kangning.qu@changhong.com>
2021-06-28 09:00:38 +0800

    Z字形变化新方法和改进方法提交

--- java/src/main/java/com/java/leet/code/ConvertZ.java
+++ java/src/main/java/com/java/leet/code/ConvertZ.java
@@ -2,15 +2,103 @@ package com.java.leet.code;
 
 /**
  * 6. Z 字形变换
- * 执行用时： 25 ms 在所有 Java 提交中击败了 14.46% 的用户
- * 内存消耗： 39.2 MB , 在所有 Java 提交中击败了 29.63% 的用户
  */
 public class ConvertZ {
 
     public static void main(String[] args) {
-        System.out.println(convert("PAYPALISHIRING", 3));
+        System.out.println(convert2New("PAYPALISHIRING", 1));
     }
 
+    /**
+     * 使用数组替代StringBuilder
+     * 执行用时： 3 ms , 在所有 Java 提交中击败了 98.58% 的用户
+     * 内存消耗： 38.6 MB , 在所有 Java 提交中击败了 84.73% 的用户
+     */
+    public static String convert2New(String s, int numRows) {
+
+        if (numRows == 1) {
+            return s;
+        }
+
+        char[] chars = new char[s.length()];
+        int arrayIndex = 0;
+        int groupCounts = (numRows << 1) -2;
+        for (int index = 0; index < numRows; index++) {
+
+            int startIndex = index;
+            int times = 1;
+
+            while (startIndex < s.length()) {
+
+                if (index == 0 || index == numRows - 1) {
+                    chars[arrayIndex] = s.charAt(startIndex);
+                    ++arrayIndex;
+
+                } else {
+                    chars[arrayIndex] = s.charAt(startIndex);
+                    ++arrayIndex;
+                    if (times*groupCounts - index >= s.length()) {
+                        break;
+                    }
+                    chars[arrayIndex] = s.charAt(times*groupCounts - index);
+                    ++arrayIndex;
+                    times++;
+                }
+
+                startIndex += groupCounts;
+            }
+        }
+
+        return new String(chars);
+    }
+
+    /**
+     * 出现的问题, 审题不清, 第一次的考虑方向与题目指定的不同
+     * 找出规律, 使用StringBuilder拼接字符串
+     * 执行用时： 4 ms , 在所有 Java 提交中击败了 85.36% 的用户
+     * 内存消耗： 8.5 MB , 在所有 Java 提交中击败了 87.58% 的用户
+     */
+    public static String convert2(String s, int numRows) {
+
+        if (numRows == 1) {
+            return s;
+        }
+
+        StringBuilder result = new StringBuilder();
+        int groupCounts = (numRows << 1) -2;
+        for (int index = 0; index < numRows; index++) {
+
+            int startIndex = index;
+            int times = 1;
+
+            while (startIndex < s.length()) {
+
+                // 第一行和最后一行都是 startIndex, startIndex + groupCounts
+                if (index == 0 || index == numRows - 1) {
+                    result.append(s.charAt(startIndex));
+
+                } else {
+                    // 中间行是 startIndex和下一循环前index处
+                    result.append(s.charAt(startIndex));
+                    if (times*groupCounts - index >= s.length()) {
+                        break;
+                    }
+                    result.append(s.charAt(times*groupCounts - index));
+                    times++;
+                }
+
+                startIndex += groupCounts;
+            }
+        }
+
+        return result.toString();
+    }
+
+    /**
+     * 构建二维数组, 存放Z字变形数据, 然后读取
+     * 执行用时： 25 ms 在所有 Java 提交中击败了 14.46% 的用户
+     * 内存消耗： 39.2 MB , 在所有 Java 提交中击败了 29.63% 的用户
+     */
     public static String convert(String s, int numRows) {
 
         int length = s.length();
@@ -95,9 +183,7 @@ public class ConvertZ {
                 if (c != '\u0000') {
                     builder.append(charArray[j][i]);
                 }
-//                System.out.print(charArray[j][i]);
             }
-//            System.out.println();
         }
 
         System.out.println(builder);
